d02059b6a24802d145d210ce90ef307097d5b8a8
Ivan Gromov <50546979+Gr0mi4@users.noreply.github.com>
2020-07-28 11:19:40 +0300

    Version update

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,9 @@
 <template>
-  <div>
-    <DataInput id="input"
-               :city="city"
-               :weather-service="weatherService"
-               @weatherServiceChanged="weatherService=$event"
-               @cityNameChanged="city = $event"/>
-    <ShowWeather :city="city" :weatherService="weatherService" />
-  </div>
+  <router-view></router-view>
 </template>
 
 <script>
-  import ShowWeather from "./components/ShowWeather";
-  import DataInput from "./components/DataInput";
-
-  export default {
-    components: {ShowWeather, DataInput},
-    data: function() {
-      return {
-        city: 'Minsk',
-        weatherService: 'openWeather',
-      }
-    },
-  }
+export default {
+  name: 'App'
+}
 </script>
-
-<style>
-  body {
-    background-image: linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%);
-    display: flex;
-    justify-content: center;
-    height: 100vh;
-    margin: 0;
-    text-align: center;
-    font-family: Bahnschrift;
-    color: #fff;
-  }
-</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,32 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Vuelidate from 'vuelidate/src'
+import VueI18n from 'vue-i18n'
+import messages from './translation/messages'
+import dateTimeFormats from './translation/dateTimeFormats'
 import App from './App.vue'
-import Input from './components/DataInput'
+import router from './router'
+import store from './store'
 
-Vue.config.productionTip = false;
-Vue.component('Input', Input);
+Vue.use(VueRouter)
+Vue.use(Vuelidate)
+Vue.use(VueI18n)
+Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+const i18n = new VueI18n({
+  fallbackLocale: 'en',
+  dateTimeFormats,
+  locale: navigator.languages[0].substr(0, 2),
+  messages
+})
+
+store.dispatch('checkAuthentication')
+  .then((user) => { if (user) { store.dispatch('getUserAdditionalInfo') } })
+  .then(() => {
+    new Vue({
+      i18n,
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  })
