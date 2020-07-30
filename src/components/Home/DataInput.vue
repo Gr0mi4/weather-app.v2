7db7312446695ce0f3e5fd6ab304323e7842e99b
Ivan Gromov <50546979+Gr0mi4@users.noreply.github.com>
2020-07-30 11:30:39 +0300

    few words to check autoupdate

--- src/components/Home/DataInput.vue
+++ src/components/Home/DataInput.vue
@@ -7,7 +7,7 @@
        v-if='$store.state.userAdditionalInfo'
        class="input-header headline"
     >{{$t('dataInput.enterCity', { userName: $store.getters.userName })}}</h1>
-    <h1>New stuff</h1>
+    <h1>New stuff here!!!</h1>
     <input
        id='city-input-field'
        class="input-field"
